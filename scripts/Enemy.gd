291974bae38afe3be183967e5e6a0176965253bb
Farah <eduardobfarah@gmail.com>
2024-04-22 23:12:39 -0300

    Adding infinite background

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,6 +1,7 @@
 class_name Enemy extends Area2D
 
 @export var speed = 200
+@export var hp = 1
 	
 func _physics_process(delta):
 	global_position.y += speed * delta
@@ -11,7 +12,14 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 func die():
 	queue_free()
 
+func take_damage(damage):
+	if hp - damage == 0:
+		die()
+	else:
+		hp -= 1
 
-func _on_area_entered(area):
-	print(area)
+func _on_body_entered(body):
+	if body is Player:
+		body.die()
+		die()
 
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,9 +1,17 @@
 extends Node2D
 
+@export var enemy_scenes: Array[PackedScene] = []
+
 @onready var player_spawn_pos = $PlayerSpawnPos
 @onready var laser_container = $LaserContainer
+@onready var timer = $EnemySpawnTimer
+@onready var enemy_container = $EnemyContainer
+@onready var pause_menu = $UILayer/PauseMenu
+@onready var pb = $ParallaxBackground
 
 var player = null
+
+var scroll_speed = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,11 +24,23 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
-		get_tree().quit()
+		pause_menu.open()
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
+		
+	if timer.wait_time > 0.5:
+		timer.wait_time =- delta*0.005
+		
+	pb.scroll_offset.y += delta * scroll_speed
+	if pb.scroll_offset.y >= 960:
+		pb.scroll_offset.y = 0
 
 func _on_player_laser_shot(laser_scene, location):
 	var laser = laser_scene.instantiate()
 	laser.global_position = location
 	laser_container.add_child(laser)
+
+func _on_enemy_spawn_timer_timeout():
+	var e = enemy_scenes.pick_random().instantiate()
+	e.global_position = Vector2(randf_range(50 , 500), -50)
+	enemy_container.add_child(e)
